eac42465288142f2d4cce24b57cbbc0d51d07d1c
hongteacher <isabel8282@gmail.com>
2022-04-11 15:40:25 +0900

    theme Color and styled-Component 수정

--- src/Main.js
+++ src/Main.js
@@ -18,57 +18,55 @@ const Main = ({onToggle, toggle}) => {
             {!toggle ? <DarkButton onClick={onToggle} /> : <LightButton onClick={onToggle} />}    
 
             </HeaderWrapper>        
-            <p>{time.toLocaleDateString()}</p>
-            <p>{time.toLocaleTimeString()}</p>
+            <DateTimer>{time.toLocaleDateString()}</DateTimer>
+            <HourTimer>{time.toLocaleTimeString()}</HourTimer>
         </Container>
     );
 };
 
 const HeaderWrapper = styled.header`
-    width : 100%;
-    height: 50px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0 20px;
-    background: ${props => props.theme.headerColor};
-    h1 {
-        color: ${props => props.theme.headerTextColor};
-        font-size: 40px;
-    }
+  width: 100%;
+  height: 50px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+  background: ${(props) => props.theme.headerColor};
+  h1 {
+    color: ${(props) => props.theme.headerTextColor};
+    font-size: 40px;
+  }
 `;
 
 const Container = styled.div`
-    width: 100%;
-    height: 100vh;
-    background : ${props => props.theme.bgcolor};
-    color : ${props => props.theme.textColor};
-    font-size: 40px;
-   p{      
-        color: #000;
-        font-size: 150px;
-        text-align: center;
-
-
-       &:last-child{
-        color: #000;
-        font-size: 200px;
-        text-align: center;
-       }
-   }
+  width: 100%;
+  height: 100vh;
+  background: ${(props) => props.theme.bgColor};
+  color: ${(props) => props.theme.textColor};
+  font-size: 40px;
 `;
 
-//html태그 이외의 스타일을 꾸며줄때는 소괄호 사용!!!
+const DateTimer = styled.p`
+  font-size: 150%;
+  text-align: center;
+`;
+
+const HourTimer = styled.p`
+  font-size: 200%;
+  text-align: center;
+`;
+
+//html 태그 이외의 스타일을 꾸며줄때는 소괄호 사용!!!
 const DarkButton = styled(MdDarkMode)`
-    font-size: 30px;
-    color : ${props => props.theme.headerTextColor};
-    cursor: pointer;
-`
+  font-size: 32px;
+  color: ${(props) => props.theme.headerTextColor};
+  cursor: pointer;
+`;
 
 const LightButton = styled(MdOutlineDarkMode)`
-    font-size: 30px;
-    color : ${props => props.theme.headerTextColor};
-    cursor: pointer;
-`
+  font-size: 32px;
+  color: ${(props) => props.theme.headerTextColor};
+  cursor: pointer;
+`;
 
 export default Main;
--- src/theme.js
+++ src/theme.js
@@ -1,15 +1,14 @@
 export const lightTheme = {
-    bgcolor : '#fff',
-    textColor : '#000',
-    headerColor : '#000',
-    headerTextColor :'#fff'
-
-};
-
-export const darkTheme = {
-    bgcolor : '#000',
-    textColor : '#fff',
-    headerColor : '#fff',
-    headerTextColor :'#000'
-};
-
+    bgColor: "#fff",
+    textColor: "#000",
+    headerColor: "#000",
+    headerTextColor: "#fff"
+  };
+  
+  export const darkTheme = {
+    bgColor: "#000",
+    textColor: "#fff",
+    headerColor: "#fff",
+    headerTextColor: "#000"
+  };
+  
